77d37547fb793c06a057555b1db37b0f58c39813
Лисник Екатерина <ekaterina.lisnik@yandex.ru>
2022-12-04 18:47:29 +0500

    Вспомогательные функции

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const getRandomNumber = (max, min = 0) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const checkMaxLenght = (testableStr, maxLenght) => {
+  if (typeof testableStr !== 'string') {
+    testableStr = String(testableStr);
+  }
+  return testableStr.lenght <= maxLenght;
+}
